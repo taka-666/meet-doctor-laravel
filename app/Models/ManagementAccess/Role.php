78428b22097f4f211dfb80954780ecfcabd4bdde
dvaa <93278631+Dava-033@users.noreply.github.com>
2024-03-17 23:16:09 +0700

    add model database

---
+++ app/Models/ManagementAccess/Role.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\ManagementAccess;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Role extends Model
+{
+    // use HasFactory;
+
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'role';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+    
+}
