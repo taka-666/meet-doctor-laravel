2a412b04fdb5020e193f4f0595c6436afaf2cddb
dvaa <93278631+Dava-033@users.noreply.github.com>
2024-03-17 23:29:34 +0700

    done add model

--- app/Models/MasterData/Consultation.php
+++ app/Models/MasterData/Consultation.php
@@ -4,8 +4,27 @@ namespace App\Models\MasterData;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Consultation extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'consultation';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
--- app/Models/MasterData/TypeUser.php
+++ app/Models/MasterData/TypeUser.php
@@ -4,8 +4,26 @@ namespace App\Models\MasterData;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class TypeUser extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'type_user';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
