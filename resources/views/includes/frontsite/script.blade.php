e90f1c50e85118f9dfab058729b5e992116f1d9c
dvaa <93278631+Dava-033@users.noreply.github.com>
2024-05-05 16:25:26 +0700

    Slicing appointment and Payment

--- resources/views/includes/frontsite/script.blade.php
+++ resources/views/includes/frontsite/script.blade.php
@@ -1,1 +1,1 @@
-<script defer src="{{ url('https://unpkg.com/alpinejs@3.8.0/dist/cdn.min.js') }}"></script>
+<script defer src="{{ url('https://unpkg.com/alpinejs@3.8.0/dist/cdn.min.js') }}"> </script>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,14 +9,17 @@
             @include('includes.frontsite.style')
         @stack('after-style')
     </head>
+
+
     <body>
         @include('components.frontsite.header')
             @yield('content')
         @include('components.frontsite.footer')
 
         @stack('before-script')
-            @include('includes.frontsite.style')
+            @include('includes.frontsite.script')
         @stack('after-script')
+
 
         {{-- Modals here --}}
         {{-- if you have a modals, create here!! --}}
